0e3402811e44d3a2263ca078a010c332510edef7
ah-james <11ahj11@gmail.com>
2021-05-13 19:02:38 -0400

    Screen Switches When Game Starts

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header'
 import StartGameScreen from './containers/StartGameScreen'
+import GameScreen from './containers/GameScreen'
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState()
+
+  const handleStartGame = selectedNumber => {
+    setUserNumber(selectedNumber)
+  }
+
+  let content = <StartGameScreen handleStartGame={handleStartGame} />
+
+  if (userNumber) {
+    content = <GameScreen userNumber={userNumber}/>
+  }
+
   return (
     <View style={styles.container}>
       <Header title="Guess a Number"/>
-      <StartGameScreen />
+      {content}
       <StatusBar style="auto" />
     </View>
   );
