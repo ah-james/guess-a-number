468b9e673e33f88d71369d2acfb6a19a3475248c
ah-james <11ahj11@gmail.com>
2021-05-12 22:17:22 -0400

    Adding Colors Constants

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { StyleSheet, View, Text } from 'react-native'
+import Colors from '../constants/colors'
 
 const Header = props => {
 
@@ -15,7 +16,7 @@ const styles = StyleSheet.create({
         width: '100%',
         height: 90,
         paddingTop: 40,
-        backgroundColor: '#BBE5ED',
+        backgroundColor: Colors.primary,
         alignItems: 'center',
         justifyContent: 'center',
     },
---
+++ constants/colors.js
@@ -0,0 +1,4 @@
+export default {
+    primary: '#FF9F1C',
+    secondary: '#2EC4B6',
+}
