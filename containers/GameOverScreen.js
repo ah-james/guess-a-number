8cbf2a9de96098000702e3b65d47498fb0501ccb
ah-james <11ahj11@gmail.com>
2021-05-14 18:17:27 -0400

    More Image Changes

--- containers/GameOverScreen.js
+++ containers/GameOverScreen.js
@@ -9,7 +9,13 @@ const GameOverScreen = props => {
     return(
         <View style={styles.screen}>
             <TitleText>Game Over!</TitleText>
-            <Image source={require('../assets/khaled.jpg')} />
+            <View style={styles.imageContainer}>
+                <Image 
+                style={styles.image} 
+                // source={require('../assets/success.jpg')} 
+                source={{uri: 'https://blueseatblogs.com/wp-content/uploads/2018/07/consciousness-709143-400x300.jpg'}}
+                />
+            </View>
             <BodyText>There Were {props.totalRounds} Rounds</BodyText>
             <BodyText>Your Number Was {props.userNumber}</BodyText>
             <Button title='New Game' onPress={props.handleNewGame} color={Colors.secondary} />
@@ -23,6 +29,19 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
     },
+    image: {
+        width: '100%',
+        height: '100%',
+    },
+    imageContainer: {
+        width: 300,
+        height: 300,
+        borderRadius: 150,
+        borderWidth: 3,
+        borderColor: 'black',
+        overflow: 'hidden',
+        marginVertical: 30,
+    }
 })
 
 export default GameOverScreen
